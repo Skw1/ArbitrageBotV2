1a96be910bd41e38df27d2181c512f28c606d588
Andrii Doluda <andrewdoluda@gmail.com>
2025-04-26 15:41:00 +0200

    add Symbols.js

---
+++ public/js/symbols.js
@@ -0,0 +1,41 @@
+/*
+  // Example Ticker 
+  const ticker = 'TRON usdt';
+
+  // Spot Symbols 
+  const mexcSpotSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX'); // BTCUSDT
+  const lbankSpotSymbol = ticker.toLowerCase().replace('usdt', '_usdt').replace(' ','').replace('tron','trx'); // btc_usdt
+  const bybitSpotSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX'); // BTCUSDT
+  const kucoinSpotSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX').replace('USDT', '-USDT'); // BTC-USDT
+  const ourbitSpotSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX'); // BTCUSDT // ?
+  const bitunixSpotSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX'); // BTCUSDT
+
+  // Futures Symbols 
+  const mexcFuturesSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX').replace('USDT', '_USDT'); // BTC_USDT
+  const lbankFuturesSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX').replace('USDT', '_USDT'); // BTC_USDT
+  const bybitFuturesSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX'); // BTCUSDT
+  const kucoinFuturesSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX').replace('USDT', '-USDT'); // BTC-USDT
+  const ourbitFuturesSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX'); // BTCUSDT // ?
+  const bitunixFuturesSymbol = ticker.toUpperCase().replace(' ','').replace('TRON','TRX'); // BTCUSDT
+
+
+  // Console Log
+  console.log("mexcSpotSymbol" ,  mexcSpotSymbol);
+  console.log("mexcFuturesSymbol" ,  mexcFuturesSymbol);
+
+  console.log("lbankSpotSymbol" ,  lbankSpotSymbol);
+  console.log("lbankFuturesSymbol" ,  lbankFuturesSymbol);
+
+  console.log("bybitSpotSymbol" ,  bybitSpotSymbol);
+  console.log("bybitFuturesSymbol" ,  bybitFuturesSymbol);
+
+  console.log("kucoinSpotSymbol" ,  kucoinSpotSymbol);
+  console.log("kucoinFuturesSymbol" ,  kucoinFuturesSymbol);
+
+  console.log("ourbitSpotSymbol" ,  ourbitSpotSymbol);
+  console.log("ourbitFuturesSymbol" ,  ourbitFuturesSymbol);
+
+  console.log("bitunixSpotSymbol" ,  bitunixSpotSymbol);
+  console.log("bitunixFuturesSymbol" ,  bitunixFuturesSymbol);
+
+*/
